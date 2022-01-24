acc532a7d79aadacecf307958045424b4b318f04
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-01-24 11:29:41 +0100

    new script : linear mixed models

---
+++ R/lmm.R
@@ -0,0 +1,9 @@
+# Linear mixed models
+
+# Last updated 2022-01-24
+
+# Links ----
+
+# https://ourcodingclub.github.io/tutorials/mixed-models/
+# https://towardsdatascience.com/how-linear-mixed-model-works-350950a82911
+# https://poissonisfish.com/2017/12/11/linear-mixed-effect-models-in-r/
