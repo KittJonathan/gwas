b722daa8638b2389c21ccdf4de9d10f3d0370bd8
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-01-24 11:39:52 +0100

    intro to lmm

--- R/lmm.R
+++ R/lmm.R
@@ -7,3 +7,7 @@
 # https://ourcodingclub.github.io/tutorials/mixed-models/
 # https://towardsdatascience.com/how-linear-mixed-model-works-350950a82911
 # https://poissonisfish.com/2017/12/11/linear-mixed-effect-models-in-r/
+
+# Introduction to linear mixed models ----
+
+# https://ourcodingclub.github.io/tutorials/mixed-models/
