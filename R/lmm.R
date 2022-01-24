8a82947396cdf66c59d199a543c5298fd7f9b7c2
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-01-24 15:16:24 +0100

    added url in links

--- R/lmm.R
+++ R/lmm.R
@@ -7,6 +7,7 @@
 # https://ourcodingclub.github.io/tutorials/mixed-models/
 # https://towardsdatascience.com/how-linear-mixed-model-works-350950a82911
 # https://poissonisfish.com/2017/12/11/linear-mixed-effect-models-in-r/
+# https://mfviz.com/hierarchical-models/
 
 # Load packages ----
 
