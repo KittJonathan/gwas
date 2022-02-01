03ab92888fbb07e74b10c76e7588e5e77104127c
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-02-01 17:14:17 +0100

    new tutorial

---
+++ R/gwas_analysis_demonstration.R
@@ -0,0 +1,13 @@
+# GWAS Analysis Demonstration in R
+
+# Last updated 2022-02-01
+
+# Links ----
+
+# https://whussain2.github.io/Materials/Teaching/GWAS_R_2.html#content
+
+# Load packages ----
+
+# Read the marker data files ----
+
+geno <- read.table("data/RiceDiversity.44K.MSU6.Genotypes_PLINK/RiceDiversity_44K_Genotypes_PLINK/sativas413.ped")
