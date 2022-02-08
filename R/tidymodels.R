f0626abd1806ee5ebeee1e6ee5c985e9024d2ad8
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-02-08 17:32:36 +0100

    update

--- R/tidymodels.R
+++ R/tidymodels.R
@@ -5,6 +5,33 @@
 # Load packages ----
 
 library(tidymodels)
+library(tidyverse)
 library(readr)
 library(broom.mixed)
 library(dotwhisker)
+
+# Get started ----
+
+# https://www.tidymodels.org/start/models/
+
+# Import the sea urchins data
+
+urchins <- read_csv("https://tidymodels.org/start/models/urchins.csv") %>% 
+  setNames(c("food_regime", "initial_volume", "width")) %>% 
+  mutate(food_regime = factor(food_regime, levels = c("Initial", "Low", "High")))
+
+# Quick look at the data
+
+urchins
+
+# Plot the data
+
+ggplot(data = urchins,
+       mapping = aes(x = initial_volume,
+                     y = width,
+                     group = food_regime,
+                     col = food_regime)) +
+  geom_point() +
+  geom_smooth(method = lm, se = FALSE) +
+  scale_colour_viridis_d(option = "plasma", end = .7) +
+  theme_minimal()
