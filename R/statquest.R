f4ffd39f9a82092c2f68042246c948f7cd636e8e
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-02-08 17:14:08 +0100

    working through StatQuest videos

--- R/statquest.R
+++ R/statquest.R
@@ -6,3 +6,8 @@
 
 # https://www.youtube.com/watch?v=u1cc1r_Y7M0
 
+mouse.data <- data.frame(
+  weight = c(0.9, 1.8, 2.4, 3.5, 3.9, 4.4, 5.1, 5.6, 6.3),
+  size = c(1.4, 2.6, 1.0, 3.7, 5.5, 3.2, 3.0, 4.9, 6.3))
+
+mouse.data
