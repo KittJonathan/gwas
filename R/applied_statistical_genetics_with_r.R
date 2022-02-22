e180d230c4a3c5ed0dc25bf66a30079d7f6d9a02
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-02-22 17:15:18 +0100

    update

--- R/applied_statistical_genetics_with_r.R
+++ R/applied_statistical_genetics_with_r.R
@@ -15,3 +15,12 @@
 # d. whole or partial genome-wide scans
 
 # 1.1.2 Genotype versus gene expression
+
+# Genotype = a categorical variable that takes on values from a predefined set of discrete characters
+# Trait  measure of disease status or disease progress
+
+# In population association studies, genotype = predictor variable & trait = dependent variable
+
+# 1.1.3 Population- versus family-based investigations
+
+# Individuals within the same family are likely to be more similar than are individuals from different families
