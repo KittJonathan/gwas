2bd9d4483fdee0aae86b7657e974f8287e9a2031
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-02-22 16:26:32 +0100

    update

--- R/applied_statistical_genetics_with_r.R
+++ R/applied_statistical_genetics_with_r.R
@@ -6,8 +6,12 @@
 
 # 1.1 Overview of population-based investigations
 
+# 1.1.1 Types of investigations
+
 # Population-based genetic association studies can be divided roughly into four categories of studies:
 # a. candidate polymorphism
 # b. candidate gene
 # c. fine mapping
 # d. whole or partial genome-wide scans
+
+# 1.1.2 Genotype versus gene expression
