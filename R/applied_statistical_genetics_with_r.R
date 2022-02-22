875e802c2bcff332a4e01386afda38bc740ff033
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-02-22 16:18:28 +0100

    update

---
+++ R/applied_statistical_genetics_with_r.R
@@ -0,0 +1,13 @@
+# Applied Statistical Genetics with R
+
+# Last updated 2022-02-22
+
+# 1. Genetic Association Studies ----
+
+# 1.1 Overview of population-based investigations
+
+# Population-based genetic association studies can be divided roughly into four categories of studies:
+# a. candidate polymorphism
+# b. candidate gene
+# c. fine mapping
+# d. whole or partial genome-wide scans
